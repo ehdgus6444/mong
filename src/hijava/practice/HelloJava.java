37d4435f665c40a129e91168df376f5844f63a5f
ehdgu <ehdgu@192.168.219.116>
2022-12-08 15:09:34 +0900

    첫번째

---
+++ src/hijava/practice/HelloJava.java
@@ -0,0 +1,21 @@
+package hijava.practice;
+
+public class HelloJava {
+
+	// 주석예시입니다
+	
+	public static void main(String[] args) {
+		HelloJava.say("Hi!!");
+		
+		System.out.println("Hello Java!!");
+	}
+	/*
+	 * 주석을 이어서 
+	 * 작성하는 경우 예시입니다
+	 */
+
+	
+	public static void say(String msg) {
+		System.out.println(msg);
+	}
+}
